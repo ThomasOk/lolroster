c586f2fa74dc38ff6a4d10824454664c8e1fda72
Thomas <ok.thomas.s@gmail.com>
2025-02-06 17:39:19 +0100

    add vite plugin commonjs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,12 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		commonjs(),
 		react(),
 		svgr({
 			// svgr options: https://react-svgr.com/docs/options/
@@ -18,6 +20,7 @@ export default defineConfig({
 			include: "**/*.svg",
 		}),
 	],
+
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
